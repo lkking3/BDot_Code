b8f7db1cdba438498b2a148398b80699f07dfabf
lkking3 <lkking3@ncsu.edu>
2025-04-03 08:47:59 -0400

    Updated files & added pandas csv reading

--- B-Dot Processor Core.py
+++ B-Dot Processor Core.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-import_directory = 'B-Dot Data/raw' #Saved as a subfile so the singular directory entry should be fine. File specificication will require manual input.
+import_directory = 'B-Dot_Data/raw' #Saved as a subfile so the singular directory entry should be fine. File specificication will require manual input.
 filenames = os.listdir(import_directory)
 
 root = tk.Tk()
@@ -16,10 +16,10 @@ selected_result = {"file_path": None}
 dropdown_menu = tk.OptionMenu(root,selected_file,*filenames)
 dropdown_menu.pack(padx=20,pady=10)
 
-def show_selection():
+def show_selection(): #Collects gui selection and saves filename as a string for loading on line 32
     selected = selected_file.get()
     full_path = f"{import_directory}/{selected}"
-    print(f"{full_path} selected.")
+    print(f"{full_path} selected.") 
     selected_result["file_path"] = full_path
     root.destroy()
     
@@ -28,4 +28,6 @@ select_button = tk.Button(root,text="Select File", command=show_selection)
 select_button.pack(pady=10)
 root.mainloop()
 
-print(selected_result['file_path'])
+
+data_frame = pd.read_csv(selected_result['file_path'])
+print(data_frame.head())
